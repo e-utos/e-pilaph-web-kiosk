e5eca82c0ef7e9cd388054d9b0073a007fe492f6
DennisOragon <dennis.pitallano@hotmail.co.uk>
2020-06-11 23:33:59 +0800

    init app

---
+++ src/EPila.Ph/EPila.Ph.Web.Kiosk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EPila.Ph.Web.Kiosk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
